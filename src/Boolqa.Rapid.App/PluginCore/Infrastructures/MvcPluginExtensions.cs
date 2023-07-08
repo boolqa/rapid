3aede30c6f93d1c72ae196b8d3f5717cd51ebd78
Vladislav Negmatulin <vlad-neg@mail.ru>
2023-07-08 22:15:00 +0300

    #2 + temp logic for check load ui parts

--- src/Boolqa.Rapid.App/PluginCore/Infrastructures/MvcPluginExtensions.cs
+++ src/Boolqa.Rapid.App/PluginCore/Infrastructures/MvcPluginExtensions.cs
@@ -1,7 +1,10 @@
-﻿using Boolqa.Rapid.App;
+﻿using System.Reflection;
+using Boolqa.Rapid.App;
 using Boolqa.Rapid.App.PluginCore.Infrastructures;
 using Boolqa.Rapid.PluginCore.Models;
+using McMaster.NETCore.Plugins;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -22,11 +25,21 @@ public static class MvcPluginExtensions
 
         foreach (var uiPlugin in uiPlugins)
         {
-            mvcBuilder.AddPluginLoader(uiPlugin.Loader!);
+            mvcBuilder.AddPluginLoader2(uiPlugin.Loader!);
 
             // For testing
             var pluginTypes = uiPlugin.LoadedAssemblies.SelectMany(a => a.GetTypes())
                 .Where(t => typeof(ComponentBase).IsAssignableFrom(t) && !t.IsAbstract)
+                .Where(t => t != null)
+                .ToArray();
+
+            // For testing2
+            var pluginTypes2 = uiPlugin.LoadedAssemblies.SelectMany(a => a.GetTypes()).ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, pluginTypes2.Select(x => x.FullName)));
+
+            var pluginTypes22 = pluginTypes2.Where(t => t.BaseType?.Name.Contains("ComponentBase", 
+                StringComparison.OrdinalIgnoreCase) == true && !t.IsAbstract)
                 .Where(t => t != null)
                 .ToArray();
 
@@ -35,6 +48,32 @@ public static class MvcPluginExtensions
                 ForTest.Types.Add(type.FullName!, type);
             }
             // For testing end
+        }
+
+        return mvcBuilder;
+    }
+
+    public static IMvcBuilder AddPluginLoader2(this IMvcBuilder mvcBuilder, PluginLoader pluginLoader)
+    {
+        var pluginAssembly = pluginLoader.LoadDefaultAssembly();
+
+        // This loads MVC application parts from plugin assemblies
+        var partFactory = ApplicationPartFactory.GetApplicationPartFactory(pluginAssembly);
+        foreach (var part in partFactory.GetApplicationParts(pluginAssembly))
+        {
+            mvcBuilder.PartManager.ApplicationParts.Add(part);
+        }
+
+        // This piece finds and loads related parts, such as MvcAppPlugin1.Views.dll.
+        var relatedAssembliesAttrs = pluginAssembly.GetCustomAttributes<RelatedAssemblyAttribute>();
+        foreach (var attr in relatedAssembliesAttrs)
+        {
+            var assembly = pluginLoader.LoadAssembly(attr.AssemblyFileName);
+            partFactory = ApplicationPartFactory.GetApplicationPartFactory(assembly);
+            foreach (var part in partFactory.GetApplicationParts(assembly))
+            {
+                mvcBuilder.PartManager.ApplicationParts.Add(part);
+            }
         }
 
         return mvcBuilder;
